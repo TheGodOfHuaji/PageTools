ef6031ada92a8d8354ec28c935ee7f29ed1e8cd4
Zhang <32956088+TheGodOfHuaji@users.noreply.github.com>
2018-08-05 11:58:04 +0800

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,8 @@
 // 夜间模式
 var nightMode = getCookie("PageTools_nm");
 if (nightMode == "true") {
-	nightModeToggle();
+	var styleStr = "*{background-color:#333!important;color:#888!important;border-color:#555!important;scrollbar-arrow-color:#ccc!important;scrollbar-base-color:#26a!important;scrollbar-shadow-color:#26a!important;scrollbar-face-color:#333!important;scrollbar-highlight-color:#26a!important;scrollbar-dark-shadow-color:#26a!important;scrollbar-3d-light-color:#26a!important;scrollbar-track-color:#333!important}a,a *{color:#8ad!important;text-decoration:none!important}a:visited,a:visited *,a:active,a:active *{color:#58a!important}a:hover,a:hover *{color:#ad8!important;background:#666!important}input,select,option,button,textarea{color:#aaa!important;background:#555!important;border:#666!important;border-color:#666 #888 #888 #666!important}input:focus,select:focus,option:focus,button:focus,textarea:focus,input:hover,select:hover,option:hover,button:hover,textarea:hover{color:#bbb!important;background:#5a5a5a!important;border-color:#777 #999 #999 #777!important}input[type=button],input[type=submit],input[type=reset],input[type=image]{border-color:#888 #666 #666 #888!important}input[type=button]:focus,input[type=submit]:focus,input[type=reset]:focus,input[type=image]:focus,input[type=button]:hover,input[type=submit]:hover,input[type=reset]:hover,input[type=image]:hover{color:#bbb!important;background:#666!important;border-color:#aaa #888 #888 #aaa!important}";
+	addStyle(styleStr,"nightMode");
 }
 
 /* HTML操作 */
